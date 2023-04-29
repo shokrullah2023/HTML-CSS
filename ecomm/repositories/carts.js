50f1c9875c6811d72f308d8f3b8ac37ee16c12a8
shokrullah2023 <shokrullahakhlaqi01@gmail.com>
2023-04-29 16:05:09 -0600

    HTML&CSS Property

--- ecomm/repositories/carts.js
+++
@@ -1,5 +0,0 @@
-const Repository = require('./repository');
-
-class cartRepository extends Repository{}
-
-module.exports = new cartRepository('carts.json');
--- node/index.js
+++
@@ -1,9 +0,0 @@
-// const message = require('./myscript.js');
-
-const counterObject = require('./myscript.js');
-console.log(counterObject.getCounter());
-console.log(counterObject.incrementCounter());
-console.log(counterObject.getCounter());
-
-const doubleCounter = require('./myscript.js');
-console.log(doubleCounter.getCounter());
